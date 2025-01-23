238db1891d073c1292713901df2b30a0e0d703aa
ayaancdry <b23me1013@iitj.ac.in>
2025-01-23 22:32:05 +0530

    ported experience_relay, set loop for multiple episodes

--- flappy_bird_gymnasium/agent.py
+++ flappy_bird_gymnasium/agent.py
@@ -2,6 +2,8 @@ import torch
 import gymnasium as gym
 import flappy_bird_gymnasium
 from dqn import DQN
+from experience_relay import ReplayMemory
+import itertools
 
 class Agent:
     '''run function will do both the training as well as run the test afterwards'''
@@ -18,6 +20,7 @@ class Agent:
 
         num_actions = env.action_space.n
         num_states = env.observation_space.shape[0]
+        rewards_per_episode = [] # to store the rewards of each episode in a list
 
         ''' 
         num_states = 12 and num_actions=2 in case of FlappyBird
@@ -26,29 +29,53 @@ class Agent:
         '''declare the policy dqn'''
         policy_dqn = DQN(num_states, num_actions)
 
+        ''' If we're training, initialise the deque, i.e, the replay memory'''
+        if is_training:
+            memory = ReplayMemory(10000)
+        
 
-        '''initialise the environment using reset()'''
-        obs, _ = env.reset()
+        for episode in itertools.count():
+            # itertools help in running the loop continuously till the time I stop it after I get decent results. 
+
+            ''' --- A single Episode begins here ---'''
+
+            '''initialise the environment using reset()'''
+            state, _ = env.reset()
+            terminated = False
+            episode_reward = 0.0 # Initialise reward in each episode to zero
 
 
-        while True:
-            action = env.action_space.sample() # sampling a random action from the action space
-            ''' 
-            The sample space here is : 
-            0 - do nothing 
-            1 - flap
-            '''
-            obs, reward,terminated, _ , info = env.step(action) 
-            ''' 
-            the action will be passed to the step() function, which in return will give us  : 
-            - observation made (what the next state is)
-            - reward for the previous action
-            - terminated will be returned TRUE if the bird hit the ground or any pipe
-            - info : given for debugging
-            '''
+            while not terminated:
+                action = env.action_space.sample() # sampling a random action from the action space
+                ''' 
+                The sample space here is : 
+                0 - do nothing 
+                1 - flap
+                '''
+                new_state, reward, terminated, _ , info = env.step(action) 
+                ''' 
+                the action will be passed to the step() function, which in return will give us  : 
+                - observation made (what the next state is)
+                - reward for the previous action
+                - terminated will be returned TRUE if the bird hit the ground or any pipe
+                - info : given for debugging
+                '''
 
-            if terminated:
-                break
+                episode_reward += reward
 
-        env.close()
+                ''' If we'e training, then append the following to the memory '''
+                if is_training:
+                    memory.append((state, action, new_state, reward, terminated))
+
+                # Move to new state. Keep track of your current state
+                state = new_state
+
+
+                # if terminated:
+                #     break
+            
+            rewards_per_episode.append(episode_reward) # Append the reward of each episode to the list
+
+            ''' --- A Single episode ends here --- '''
+
 
